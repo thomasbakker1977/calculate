45ac0c4821ae0a0ac38962576e6194cff14fc64c
thomasbakker1977 <113938575+thomasbakker1977@users.noreply.github.com>
2023-01-13 23:22:23 +0100

    Named the variables

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ __winc_id__ = '499e67d5cb54448e93cee7465be2c866'
 __human_name__ = 'calculate'
 
 # Add your code after this line
+Price_Broccoli = 2 
+Price_Leek = 2
+Price_Potato = 3
+Price_Brussel_Sprout = 7
