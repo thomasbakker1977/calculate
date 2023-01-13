9fefc4ab80c1ea3ae4696e6bece6dd9391b41dde
thomasbakker1977 <113938575+thomasbakker1977@users.noreply.github.com>
2023-01-13 23:48:59 +0100

    Added the totalprice and the average price.

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ Price_Broccoli = 2
 Price_Leek = 2
 Price_Potato = 3
 Price_Brussel_Sprout = 7
+
+# Calculate the total price
+sum_one_each = Price_Broccoli + Price_Leek + Price_Potato + Price_Brussel_Sprout
+
+print ("The total sum is: " + str(sum_one_each)) # Combining string with integer (convert int to str)
+
+
+# Calculate the average price per item
+avg_price = sum_one_each / 4
+
+print ("The average price is: " + str(avg_price))
+
+
